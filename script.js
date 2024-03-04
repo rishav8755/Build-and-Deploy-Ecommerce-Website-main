3226e399ffee6adefb0089bd4454ce1fa4e95284
rishav8755 <rishavpatel654@gmail.com>
2024-03-04 12:03:58 +0530

    vvvv

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// Script for navigation bar
+
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
-const nav = document.getElementById('navbar')
+const nav = document.getElementById('navbar');
 if (bar) {
     bar.addEventListener('click',() => {
         nav.classList.add('active');
